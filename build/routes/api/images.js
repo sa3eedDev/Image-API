b0ee577018ea850d3fb6b965d7e5be1a3143eb48
Sa3eedDev <36833212+sa3eedDev@users.noreply.github.com>
2022-10-19 12:38:00 -0400

    cleaned repo

--- build/routes/api/images.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var images_1 = require("../../controllers/images");
-var router = (0, express_1.Router)();
-// use Middlewares to check input file, cache, resize
-router.get('/', images_1.checkInput, images_1.checkCache, images_1.resize);
-exports.default = router;
